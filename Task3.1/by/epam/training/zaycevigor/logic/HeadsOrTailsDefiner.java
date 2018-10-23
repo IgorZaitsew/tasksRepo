c348ce40ec88499bf7c2519ccde100927cbd5342
IgorZaitsew <igor42638@gmail.com>
2018-10-23 11:31:24 +0300

    Task 3.1 is added

---
+++ Task3.1/by/epam/training/zaycevigor/logic/HeadsOrTailsDefiner.java
@@ -0,0 +1,7 @@
+package by.epam.training.zaycevigor.logic;
+
+public class HeadsOrTailsDefiner {
+    public static boolean getHeadOrTailBoolean() {
+        return Math.random() < 0.5;
+    }
+}
---
+++ Task3.1/by/epam/training/zaycevigor/view/ConsoleOutput.java
@@ -0,0 +1,7 @@
+package by.epam.training.zaycevigor.view;
+
+public class ConsoleOutput {
+    public static void output(int head,int tail){
+        System.out.println("Heads count: "+head+"\nTails count: "+tail);
+    }
+}
